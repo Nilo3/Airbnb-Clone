3d3e3680e10d8dd5729d0977fdce34e5961f6464
Nilo3 <nilo.echezarreta@gmail.com>
2023-06-08 08:19:06 -0300

    Navbar finalizada y probada

--- airbnb-clone/app/layout.tsx
+++ airbnb-clone/app/layout.tsx
@@ -1,5 +1,5 @@
 import { Nunito } from 'next/font/google'
-
+import Navbar from './components/navbar/Navbar'
 import './globals.css'
 
 
@@ -23,7 +23,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={font.className}>{children}</body>
+      <body className={font.className}>
+        <Navbar />
+        {children}</body>
     </html>
   )
 }
--- airbnb-clone/app/page.tsx
+++ airbnb-clone/app/page.tsx
@@ -2,6 +2,6 @@
 
 export default function Home() {
   return (
-    <div className="p-4 text-8xl">Hello Airbnb!</div>
+    <div className="text-rose-500 text-6xl">Hello Airbnb!</div>
   )
 }
