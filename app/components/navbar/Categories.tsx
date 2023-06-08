a0b708d94f449d36929c0880fed4883f62ccf9f9
Valen <valenzalazar3@gmail.com>
2023-06-08 12:23:01 -0300

    merienda

---
+++ app/components/navbar/Categories.tsx
@@ -0,0 +1,8 @@
+const Categories = () => {
+    return(
+        <div></div>
+    )
+}
+
+
+export default Categories
--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -2,6 +2,7 @@
 
 import React from 'react'
 import Container from '../Container'
+import Categories from './Categories'
 import Logo from "./Logo"
 import Search from "./Search"
 import UserMenu from './UserMenu'
@@ -29,6 +30,7 @@ const NavBar = () => {
             </div>
          </Container>
         </div>
+        <Categories/>
     </div>
   )
 }
