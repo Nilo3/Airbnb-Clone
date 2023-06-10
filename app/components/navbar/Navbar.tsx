f9362a0678c4c1d5bfd18ee965028ef29ed9ac68
frannpor <frannporciel@gmail.com>
2023-06-10 14:33:11 -0300

    added categoryList display

--- app/components/navbar/Navbar.tsx
+++ app/components/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Logo from "./Logo"
 import Search from "./Search"
 import UserMenu from './UserMenu'
 import { SafeUser } from '@/app/types';
+import Categories from './Categories';
 
 interface NavbarProps {
   currentUser?: SafeUser | null;
@@ -36,6 +37,7 @@ const NavBar: React.FC<NavbarProps> = ({
             </div>
          </Container>
         </div>
+        <Categories  />
     </div>
   )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,13 @@
 import { Nunito } from 'next/font/google'
-import Navbar from './components/navbar/Navbar'
 import './globals.css'
+
+import Navbar from './components/navbar/Navbar'
 import ClientOnly from './components/ClientOnly'
+
 import RegisterModal from './components/modals/RegisterModal'
 import LoginModal from './components/modals/LoginModal'
+import RentModal from './components/modals/RentModal'
+
 import ToasterProvider from './providers/ToasterProvider'
 import getCurrentUser from './actions/getCurrentUser'
 import { Inter } from 'next/font/google'
@@ -31,6 +35,7 @@ export default async function RootLayout({
       <body className={font.className}>
         <ClientOnly>
           <ToasterProvider />
+          <RentModal />
           <RegisterModal />
           <LoginModal />
           <Navbar currentUser={currentUser} />
