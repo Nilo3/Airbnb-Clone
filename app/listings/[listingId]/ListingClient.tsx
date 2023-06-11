a1ddf6a508709b115fe8e9744ca9a645de0b2d7c
frannpor <frannporciel@gmail.com>
2023-06-11 13:20:58 -0300

    finished listingDetail

--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Reservation } from "@prisma/client";
 import { SafeListing, SafeUser } from "@/app/types";
@@ -6,6 +6,7 @@ import { useMemo } from "react";
 import { categories } from "@/app/components/navbar/Categories";
 import Container from "@/app/components/Container";
 import ListingHead from "../../components/listings/ListingHead";
+import ListingInfo from "@/app/components/listings/ListingInfo";
 
 interface ListingClientProps {
   reservations?: Reservation[];
@@ -27,11 +28,31 @@ const ListingClient: React.FC<ListingClientProps> = ({
       <div className="max-w-screen-lg mx-auto">
         <div className="flex flex-col gap-6">
           <ListingHead
-            id={"falopa"}
+            id={listing.id}
             title={listing.title}
             imageSrc={listing.imageSrc}
             locationValue={listing.locationValue}
+            currentUser={currentUser}
           />
+          <div
+            className="
+          grid 
+          grid-cols-1 
+          md:grid-cols-7 
+          md:gap-10 
+          mt-6
+          "
+          >
+            <ListingInfo
+              user={listing.user}
+              category={category}
+              description={listing.description}
+              roomCount={listing.roomCount}
+              bathroomCount={listing.bathroomCount}
+              guestCount={listing.guestCount}
+              locationValue={listing.locationValue}
+            />
+          </div>
         </div>
       </div>
     </Container>
