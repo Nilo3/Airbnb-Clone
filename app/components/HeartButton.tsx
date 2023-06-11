82b08c843b3cac7966c2e3bea90a017419c89c75
Nilo Echezarreta <nilo.echezarreta@gmail.com>
2023-06-11 01:43:30 -0300

    Favoriting functionality with toast

--- app/components/HeartButton.tsx
+++ app/components/HeartButton.tsx
@@ -1,50 +1,44 @@
-"use client" ;
-
+import React from "react";
 import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 import { SafeUser } from "../types";
+import useFavorite from "../hooks/useFavorite";
 
 interface HeartButtonProps {
-    listingId: string;
-    currentUser?: SafeUser | null ;
+  listingId: string;
+  currentUser?: SafeUser | null;
 }
 
-
-const HeartButton: React.FC<HeartButtonProps> = ({
+const HeartButton: React.FC<HeartButtonProps> = ({ listingId, currentUser }) => {
+  const { hasFavorited, toggleFavorite } = useFavorite({
     listingId,
-    currentUser
-}) => {
-    const hasFavorited = false;
-    const toggleFavorite = () => {
+    currentUser,
+  });
 
-    }
+  return (
+    <div
+      onClick={toggleFavorite}
+      className="
+          relative
+          hover:opacity-80
+          transition
+          cursor-pointer
+      "
+    >
+      <AiOutlineHeart
+        size={28}
+        className="
+            fill-white
+            absolute
+            -top-[2px]
+            -right-[2px]
+        "
+      />
+      <AiFillHeart
+        size={24}
+        className={hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"}
+      />
+    </div>
+  );
+};
 
-    return (
-        <div
-            onClick={toggleFavorite}
-            className="
-                relative
-                hover:opacity-80
-                transition
-                cursor-pointer
-            "
-        >
-            <AiOutlineHeart
-                size={28}
-                className="
-                    fill-white
-                    absolute
-                    -top-[2px]
-                    -right-[2px]
-                "
-            />
-            <AiFillHeart 
-                size={24}
-                className={
-                    hasFavorited ? "fill-rose-500" : "fill-neutral-500/70"
-                }
-            />
-        </div>
-    )
-}
-
-export default HeartButton
+export default HeartButton;
